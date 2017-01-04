0a96f2d7b7ade21fc7f8475112237b5fab679f63
Jasper <jshumakerpruitt@gmail.com>
2017-01-04 19:50:55 +0700

    User validation and validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { FactoryGirl.build(:user) }
+
   it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password) }
   it { should have_secure_password }
+
 end
