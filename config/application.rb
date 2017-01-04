f5c92fc588f84efc71a0f9f7f842841f5243cc50
Jasper <jshumakerpruitt@gmail.com>
2017-01-04 19:29:05 +0700

    enable cors

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,16 @@ module ContactsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Enable CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 :headers => :any,
+                 :methods => [:put, :delete, :get, :post, :options]
+      end
+    end
+
   end
 end
